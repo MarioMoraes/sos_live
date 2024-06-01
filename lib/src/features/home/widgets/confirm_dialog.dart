6c2c79b2ea880b4b91fc8048216962afd08ff428
Mario Moraes <moraes_mario@hotmail.com>
2024-06-01 06:17:00 -0300

    Change

--- lib/src/features/home/widgets/confirm_dialog.dart
+++ lib/src/features/home/widgets/confirm_dialog.dart
@@ -6,6 +6,44 @@ class ConfirmDialog extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text('Deseja Realmente Ativar o SOS ???'),
+          const SizedBox(
+            height: 30,
+          ),
+          TweenAnimationBuilder<double>(
+            tween: Tween(
+              begin: 0,
+              end: 5,
+            ),
+            duration: const Duration(seconds: 5),
+            builder: (BuildContext context, double value, Widget? child) {
+              return SizedBox(
+                height: 150,
+                width: 150,
+                child: Stack(
+                  fit: StackFit.expand,
+                  alignment: Alignment.center,
+                  children: [
+                    CircularProgressIndicator(
+                      color: Colors.red,
+                      strokeWidth: 10,
+                      value: value / 5,
+                    ),
+                    Center(
+                        child: Text(
+                      value.toStringAsFixed(0),
+                      style: const TextStyle(fontSize: 30),
+                    )),
+                  ],
+                ),
+              );
+            },
+          ),
+        ],
+      ),
       actions: [
         ElevatedButton(
             onPressed: () {

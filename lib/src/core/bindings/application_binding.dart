531662354fa319232af34e053efeb193e840b8ed
Mario Moraes <moraes_mario@hotmail.com>
2024-06-02 08:49:36 -0300

    Changes

--- lib/src/core/bindings/application_binding.dart
+++ lib/src/core/bindings/application_binding.dart
@@ -1,11 +1,15 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_getit/flutter_getit.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class ApplicationBinding extends ApplicationBindings {
   @override
   List<Bind<Object>> bindings() => [
         Bind.lazySingleton<Dio>((i) => Dio()),
-        Bind.lazySingleton<AudioPlayer>((i) => AudioPlayer()),
+        Bind.lazySingleton<AudioPlayer>(
+            (i) => AudioPlayer()..setReleaseMode(ReleaseMode.loop)),
+        Bind.lazySingleton<FlutterSecureStorage>(
+            (i) => const FlutterSecureStorage()),
       ];
 }

98c9dd159d6c810f39159011a805eb350a5b4441
Mario Moraes <moraes_mario@hotmail.com>
2024-06-08 06:50:20 -0300

    Changes

--- lib/src/core/bindings/application_binding.dart
+++ lib/src/core/bindings/application_binding.dart
@@ -2,6 +2,7 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_getit/flutter_getit.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:projeto_sos/src/core/interceptor/b4a_intercept.dart';
 import 'package:projeto_sos/src/repositories/user_repository.dart';
 import 'package:projeto_sos/src/repositories/user_repository_impl.dart';
 import 'package:projeto_sos/src/services/auth_service.dart';
@@ -9,6 +10,7 @@ import 'package:projeto_sos/src/services/auth_service.dart';
 class ApplicationBinding extends ApplicationBindings {
   @override
   List<Bind<Object>> bindings() => [
+        Bind.lazySingleton<Dio>((i) => Dio()..interceptors.add(B4aIntercept())),
         Bind.lazySingleton<Dio>((i) => Dio()),
         Bind.lazySingleton<AudioPlayer>(
             (i) => AudioPlayer()..setReleaseMode(ReleaseMode.loop)),
